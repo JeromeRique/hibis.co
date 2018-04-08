0555003d0589a547dbb4afb78dcad05f05063b3a
shottah <m.abrahamtt@gmail.com>
2018-04-08 15:01:32 -0400

    added about page content

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+// import { VideoPlayer, VideoOptions } from '@ionic-native/video-player';
 /**
  * Generated class for the AboutPage page.
  *
@@ -14,12 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'about.html',
 })
 export class AboutPage {
+  // vidOpts: VideoOptions;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams) {
+      //...
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
   }
+
+  // playVideo () {
+  //   this.vidOpts = {volume: 1.0};
+  //   this.vidPlayer.play('https://www.youtube.com/watch?v=JpP5FC-flyo').then(() => {
+  //     // Toast Video End
+  //   }).catch(err => {
+  //     console.log(err);
+  //   });
+  // }
+
+  // stopVideo () {
+  //   this.vidPlayer.close();
+  // }
 
 }
