eabb8bad8fa765980239d43e4fe2badcf23e2b17
shottah <m.abrahamtt@gmail.com>
2018-04-10 22:42:41 -0400

    added google map

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+declare var google: any;
 /**
  * Generated class for the ContactPage page.
  *
@@ -15,11 +16,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('map') mapEl: ElementRef;
+  map: any;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
+  
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
+    this.initMap();
+  }
+
+  initMap () {
+    let geoLoc = new google.maps.LatLng(10.663577, -61.450073);
+    
+    let mapOpt = {
+      center: geoLoc,
+      zoom: 15,
+      mapTypeId: 'hybrid',
+      streetViewControl: false
+    }
+
+    this.map = new google.maps.Map(this.mapEl.nativeElement, mapOpt);
+    this.addMarker(geoLoc, this.map)
+  }
+
+  addMarker (position, map) {
+    return new google.maps.Marker({
+      position,
+      map
+    });
   }
 
 }
